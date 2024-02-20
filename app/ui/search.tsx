ac2b5c3f66f1b67cd9ae40fceaed4823b7f1daf1
abiolatom <107856136+abiolatom@users.noreply.github.com>
2024-02-20 16:46:13 +0200

    pagination is done

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,9 +9,10 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const { replace } = useRouter();
   
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
     console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     
         if (term) {
       params.set('query', term);
@@ -19,7 +20,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
