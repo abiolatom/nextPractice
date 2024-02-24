5708cbcc37aae04b4e9e3e9d48cf3c9505859c38
abiolatom <107856136+abiolatom@users.noreply.github.com>
2024-02-24 11:29:13 +0200

    server action done

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,3 +1,35 @@
 'use server';
- 
-export async function createInvoice(formData: FormData) {}
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+// to clear cache and trigger a new request to the server
+import { revalidatePath } from 'next/cache'; 
+import { redirect } from 'next/navigation';
+
+// Validation using zod
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+  });
+   
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+  
+
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+    revalidatePath('/dashboard/invoices'); //Cache clearing
+    redirect('/dashboard/invoices');
+  }
